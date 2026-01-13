ef8e290afe4f15249cbfd08b1736d73ba9c4fd86
Asser Soliman <asserehab2612@gmail.com>
2026-01-13 13:29:00 +0100

    Adding Scripts

---
+++ Scripts/start.sh
@@ -0,0 +1,9 @@
+docker run -it \
+  --network host \
+  -e DISPLAY=:0 \
+  -e WAYLAND_DISPLAY=wayland-0 \
+  -e XDG_RUNTIME_DIR=/mnt/wslg/runtime-dir \
+  -e QT_QPA_PLATFORM=xcb \
+  -v /mnt/wslg:/mnt/wslg \
+  -v /mnt/wslg/.X11-unix:/tmp/.X11-unix \
+  moblab
